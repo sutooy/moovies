cb3a4912d1c2ab6b1e13dd8ccd7b15a5e1637921
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2024-03-24 00:50:32 +0700

    chore: adjust tailwind config & sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white-trans": 'rgba(255, 255, 255, 0.05)',
+        "dark-trans": 'rgba(0, 0, 0, 0.5)',
+        "red-brown": 'rgba(231, 76, 60, 1)',
+
+      }
+    },
   },
   plugins: [],
 }
