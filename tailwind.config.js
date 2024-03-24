c849f60ad50d83d61e4913dfa8003a7c353e6e84
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2024-03-24 17:34:05 +0700

    chore: add color tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         "white-trans": 'rgba(255, 255, 255, 0.05)',
         "dark-trans": 'rgba(0, 0, 0, 0.5)',
         "red-brown": 'rgba(231, 76, 60, 1)',
+        red: `rgba(255, 0, 0, 1)`,
+
 
       }
     },
