61a348901286aeb5fdb25486a3422d861572f702
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2024-03-25 02:56:32 +0700

    build:react-slick

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "dark-trans": 'rgba(0, 0, 0, 0.5)',
         "red-brown": 'rgba(231, 76, 60, 1)',
         red: `rgba(255, 0, 0, 1)`,
-
+        gray: "rgba(146, 146, 146, 1)"
 
       }
     },
